92815028a72806a54db726ddbdc6220d0765c3ca
Reshilobeth <92940855+Reshilobeth@users.noreply.github.com>
2022-09-23 11:52:36 +0530

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import Main from '../components/Main'
+import Row from '../components/Row'
+import requests from '../Request'
+
+export default function Home() {
+  return (
+    <div>
+      <Main />
+      <Row rowID = '1' title ='Trending' fetchURL={requests.requestTrending} />
+      <Row rowID = '2' title ='Popular' fetchURL={requests.requestPopular} />
+      <Row rowID = '3' title ='Upcoming' fetchURL={requests.requestUpcoming} />
+      <Row rowID = '4' title ='Top rated' fetchURL={requests.requestTopRated} />
+      <Row rowID = '5' title ='Horror' fetchURL={requests.requestHorror} />
+    </div>
+  )
+}
